678fc166303f895a1b3a0729f018b50736defe41
gagecantrelle <gagecantrelle1899@gmail.com>
2022-10-19 15:05:08 +0000

    asd

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -319,7 +319,7 @@ var flatten = function(arrays) {
 
 // 30. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
-var letterTally = function(str, obj) {
+var letterTally = function(str, obj={}) {
   if(str.length === 0){
     return obj;
   }
