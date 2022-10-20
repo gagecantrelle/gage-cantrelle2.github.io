d260cc9b2917099edc5a0920011c6c147a3d8124
gagecantrelle <gagecantrelle1899@gmail.com>
2022-10-20 21:47:38 +0000

    asd

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -3,22 +3,34 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 function range(start, end, int) {
+  //create an empty array
   var arr = [];
+  //check if start and end are equal to each other
   if(start === end){
+    //return arr
     return arr;
   }
+  //check if int has a value
   if(int !== undefined){
+    //check if int is negative
     if(int < 0){
+      //return arr
       return arr;
     }
-    for(let i = start; start <= end; i += int){
+    //loop from start to end and increase i by int
+  for(let i = start; i <= end; i += int){
+      //push the curent value of i into arr
       arr.push(i);
    }
+   //run if int is not defined
  }else{
-   for(let i = start; start <= end; i++){
+  //loop from start to end
+   for(let i = start; i <= end; i++){
+    //push the curent value of i into arr
      arr.push(i);
  }
  }
+ //return arr
 return arr;
 }
 
@@ -34,6 +46,7 @@ function sum(arr) {
     //add the curent value into total
     total += arr[i]; 
   }
+  //return total
 return total;
 }
 
@@ -67,10 +80,14 @@ return arr.reverse();
 ////////////////////////////////////////////////////////////////////////////////
 
 function arrayToList(array) {
+  //create a rest varaible that is equal to null
   var rest = null;
+  //loop through the given array length
   for(let i = array.length -1; i >= 0; i--){
+    //set rest to equal a object that has a key of value that equal i and a key of rest that equal rest
     rest = {value: array[i], rest: rest};
   }
+  //return rest
 return rest;
 }
 
@@ -79,14 +96,18 @@ return rest;
 ////////////////////////////////////////////////////////////////////////////////
 //list is a object
 function listToArray(list, arr=[]) {
+  //check if the given list key value of rest is equal to null
 if(list.rest === null){
+  //push the given list key value of value into arr
   arr.push(list.value);
+  //return arr
   return arr;
 }
+//push the list key value of value into arr
 arr.push(list.value);
-
+//get rid of the list key of value
 delete list.value;
-
+//return the function that has the  list paramter now equlaing the key of rest and not making any changes to arr
 return listToArray(list.rest, arr);
 }
 
@@ -94,32 +115,63 @@ return listToArray(list.rest, arr);
 // prepend /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function prepend(value, func) {
-
+function prepend(value, obj) {
+  //create an varaible that will hold the give object as a array
+  var arr = listToArray(obj);
+  //add the given value to arr
+  arr.unshift(value);
+  //return arr as an object
+  return arrayToList(arr);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // nth /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function nth(arr, target) {
+function nth(obj, value) {
+  //create a variable that holds the given object as an array
+  var arr = listToArray(obj);
   //check if given targeted index has a value
-  if(arr[target] !== undefined){
+  if(arr[value] !== undefined){
     //return the curent value in index
-    return arr[target];
-  }else{
-    return -1;
+    return arr[value];
+    //check if the given value is negative
+  }else if(value < 0){
+    //return undefined
+    return undefined;
+    //run if both check are false
   }
-
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function deepEqual() {
-
+function deepEqual(x, y) {
+// determine if x + y
+if (typeof x !== 'object' && typeof y !== 'object'){
+  return x === y;
 }
+  if (typeof x !== 'object' || typeof y !== 'object'){
+    return false
+  }
+// we know we are dealing with complex datatypes
+// create array of keys
+let xKeys = Object.keys(x);
+let yKeys = Object.keys(y);
+//check for length
+if (xKeys.length !== yKeys.length){
+  return false;
+}
+// iterate through x keys array;
+for (let i = 0; i < xKeys.length; i++){
+  if(!yKeys.includes(xKeys[i]) || !deepEqual(x[xKeys[i]], y[xKeys[i]])){
+    return false;
+}
+}
+return true;
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
